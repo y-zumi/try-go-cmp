89d1f379ce6102419484fd1965692fa54cf15727
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2019-08-11 16:41:03 +0900

    add some test case

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 type Item struct {
-	ID    string
-	Name  string
-	token string
+	ID         string
+	Name       string
+	secretCode int64
 }
 
-func GetItem(id, name string) *Item {
-	token := "token"
-	return &Item{
-		ID:    id,
-		Name:  name,
-		token: token,
+func GetItem(id, name string) Item {
+	return Item{
+		ID:         id,
+		Name:       name,
+		secretCode: 12345,
 	}
 }
--- main_test.go
+++ main_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
-func TestGet(t *testing.T) {
+func TestGetAllowUnexported(t *testing.T) {
 	type args struct {
 		id   string
 		name string
@@ -14,17 +16,17 @@ func TestGet(t *testing.T) {
 
 	testCases := map[string]struct {
 		args args
-		want *Item
+		want Item
 	}{
 		"success": {
 			args: args{
 				id:   "100001",
 				name: "test",
 			},
-			want: &Item{
-				ID:    "100001",
-				Name:  "test",
-				token: "token",
+			want: Item{
+				ID:         "100001",
+				Name:       "test",
+				secretCode: int64(12345),
 			},
 		},
 	}
@@ -32,8 +34,75 @@ func TestGet(t *testing.T) {
 	for n, tc := range testCases {
 		t.Run(n, func(t *testing.T) {
 			got := GetItem(tc.args.id, tc.args.name)
-			opt := cmp.AllowUnexported(*got)
+			opt := cmp.AllowUnexported(got)
 			if diff := cmp.Diff(got, tc.want, opt); diff != "" {
+				t.Fatalf("GetItem() = %v, want = %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestGetIgnoreUnexported(t *testing.T) {
+	type args struct {
+		id   string
+		name string
+	}
+
+	testCases := map[string]struct {
+		args args
+		want Item
+	}{
+		"success": {
+			args: args{
+				id:   "100001",
+				name: "test",
+			},
+			want: Item{
+				ID:         "100001",
+				Name:       "test",
+				secretCode: int64(12345),
+			},
+		},
+	}
+
+	for n, tc := range testCases {
+		t.Run(n, func(t *testing.T) {
+			got := GetItem(tc.args.id, tc.args.name)
+			opt := cmpopts.IgnoreUnexported(got)
+			if !cmp.Equal(got, tc.want, opt) {
+				t.Fatalf("GetItem() = %v, want = %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestGetDeepEqual(t *testing.T) {
+	type args struct {
+		id   string
+		name string
+	}
+
+	testCases := map[string]struct {
+		args args
+		want Item
+	}{
+		"success": {
+			args: args{
+				id:   "100001",
+				name: "test",
+			},
+			want: Item{
+				ID:         "100001",
+				Name:       "test",
+				secretCode: int64(12345),
+			},
+		},
+	}
+
+	for n, tc := range testCases {
+		t.Run(n, func(t *testing.T) {
+			got := GetItem(tc.args.id, tc.args.name)
+			if !reflect.DeepEqual(got, tc.want) {
 				t.Fatalf("GetItem() = %v, want = %v", got, tc.want)
 			}
 		})
